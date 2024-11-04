7c9682bf2de2a452237818429a6fefa83e965d57
Orlando Salgado <osp11@yahoo.com>
2024-11-04 13:48:04 -0800

    1st working version

--- src/tests/test_login_page.py
+++ src/tests/test_login_page.py
@@ -1,24 +1,19 @@
-from unittest import TestCase
-from home_page import HomePage
+from selenium import webdriver
+from src.pages.home_page import Homepage
+from src.pages.sign_in_page import SignInPage
 
 
-url = "https://www.redfin.com/"
-email = "xxxxxx@gmail.com"
-password = "xxxxxx"
+def test_browserstack():
+    driver = webdriver.Chrome()
+    driver.get("https://bstackdemo.com/")
 
-class LoginPage(TestCase):
+    home_page = Homepage(driver)
+    sign_in_page = SignInPage(driver)
 
-    def test_login(self):
+    home_page.click_sign_in()
 
-        home_page = HomePage()
-        home_page.get_url(url)
-        home_page.click_login_button()
-        home_page.enter_email(email)
-        home_page.click_cont_with_email()
-        home_page.click_sign_in_with_email()
-        home_page.enter_password(password)
-        home_page.click_second_sign_in_with_email()
-        title = home_page.get_page_title()
-
-        expected_title = "Real Estate, Homes for Sale, MLS Listings, Agents | Redfin"
-        assert title == expected_title, f"expected title={expected_title}, got title={title}"
+    sign_in_page.select_username()
+    sign_in_page.select_password()
+    sign_in_page.click_login()
+    home_page.get_username()
+    driver.quit()
